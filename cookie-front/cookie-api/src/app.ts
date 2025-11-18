5cc74cd80f5cc96720cdb7b1c9b501023c699327
Sowparna Shome <soupornoshome@gmail.com>
2025-11-18 23:14:58 +0530

    port change

--- cookie-front/cookie-api/src/app.ts
+++ cookie-front/cookie-api/src/app.ts
@@ -5,7 +5,7 @@ import cookieParser from "cookie-parser";
 
 dotenv.config();
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 6789;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
