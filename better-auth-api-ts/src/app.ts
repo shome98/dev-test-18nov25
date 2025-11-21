b85171af3e1aaaa268e8486ab007ea7842a3cd56
Sowparna Shome <soupornoshome@gmail.com>
2025-11-22 01:03:48 +0530

    auth use with routes

--- better-auth-api-ts/src/app.ts
+++ better-auth-api-ts/src/app.ts
@@ -1,19 +1,33 @@
 import express from "express";
-import dotenv from "dotenv";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import { fromNodeHeaders, toNodeHandler } from "better-auth/node";
+import { auth } from "./auth";
+import config from "./environment-variables";
 
-dotenv.config();
 const app = express();
-const port = process.env.PORT || 3000;
-
+const port = config.PORT || 3000;
+const originFrontEnd = config.TRUSTED_ORIGIN_URL2;
+app.use(
+  cors({
+    origin: originFrontEnd,
+    credentials: true,
+  })
+);
+app.all("/api/auth/*splat", toNodeHandler(auth));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(cors());
+
 app.use(cookieParser());
 
 app.get("/", (req, res) => {
-  res.send("Hello, world!");
+  res.send("Hello, please use /api/auth/* routes!");
+});
+app.get("/api/me", async (req, res) => {
+  const session = await auth.api.getSession({
+    headers: fromNodeHeaders(req.headers),
+  });
+  return res.json(session);
 });
 
 app.listen(port, () => {
