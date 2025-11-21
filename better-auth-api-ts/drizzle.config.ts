96bf72469c98a0778c4c32ac390f614c772e73f5
Sowparna Shome <soupornoshome@gmail.com>
2025-11-22 00:58:15 +0530

    to index

--- better-auth-api-ts/drizzle.config.ts
+++ better-auth-api-ts/drizzle.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "drizzle-kit";
 export default defineConfig({
   out: "./drizzle",
-  schema: "./src/models/schema.ts",
+  schema: "./src/models/index.ts",
   dialect: "postgresql",
   dbCredentials: {
     url: process.env.DATABASE_URL!,
