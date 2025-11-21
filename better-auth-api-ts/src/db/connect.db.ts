84181d1a1f92a82c5aa3783254eb8866a5270627
Sowparna Shome <soupornoshome@gmail.com>
2025-11-22 00:59:01 +0530

    use from env variables

--- better-auth-api-ts/src/db/connect.db.ts
+++ better-auth-api-ts/src/db/connect.db.ts
@@ -1,5 +1,7 @@
 import { neon } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-http";
-const sql = neon(process.env.DATABASE_URL!);
+import config from "../environment-variables";
+
+const sql = neon(config.DATABASE_URL);
 const db = drizzle({ client: sql });
 export default db;
